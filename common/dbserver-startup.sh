b28166e7679c12bc68e58850986dd5ea7d3ed59b
Robert Hagen <rdh@stealthllama.org>
2019-04-19 14:52:10 -0400

    Updated startup scripts

--- common/dbserver-startup.sh
+++ common/dbserver-startup.sh
@@ -5,7 +5,7 @@ zone=$(basename $temp)
 FW_IP=$(gcloud compute instances describe vm-series --zone=$zone --format="value(networkInterfaces[3].networkIP)")
 while true
     do
-        resp=$(curl -s -S -g --insecure "https://$FW_IP/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
+        resp=$(curl -s -S -g --insecure "https://$FW_IP/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key=LUFRPT1reXlBaWVoVmJoSUJ4K1RPejEwMUpveFUzRWM9MFNNYTI2OWVNaGpKblB0R1JLUjYxRnRFaGFWQkgzV08wNkt6NSt3bHNZcz0=")
 	echo $resp
         if [[ $resp == *"[CDATA[yes"* ]] ; then
             break
@@ -23,8 +23,8 @@ while true
   done
 sudo apt-get update
 sudo apt-get -y install debconf-utils
-echo "mysql-server mysql-server/root_password password paloalto@123" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password paloalto@123" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password password PaloAlto123!" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password PaloAlto123!" | sudo debconf-set-selections
 DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 mysql -uroot -ppaloalto@123 -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mysql -uroot -ppaloalto@123 -e "DELETE FROM mysql.user WHERE User='';"
@@ -33,6 +33,6 @@ mysql -uroot -ppaloalto@123 -e "FLUSH PRIVILEGES;"
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo systemctl restart mysql
 mysql -uroot -ppaloalto@123 -e "CREATE DATABASE IF NOT EXISTS Demo;"
-mysql -uroot -ppaloalto@123 -e "CREATE USER 'demouser'@'%' IDENTIFIED BY 'paloalto@123';"
+mysql -uroot -ppaloalto@123 -e "CREATE USER 'demouser'@'%' IDENTIFIED BY 'PaloAlto123!';"
 mysql -uroot -ppaloalto@123 -e "GRANT ALL PRIVILEGES ON Demo.* TO 'demouser'@'%';"
 mysql -uroot -ppaloalto@123 -e "FLUSH PRIVILEGES;"
