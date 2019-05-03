5b74c09c3b6291a6fd480ef76826aecf1ea97b03
Michael Richardson <mirichardson@paloaltonetworks.com>
2019-05-03 15:37:39 -0400

    Switch to static AMIs.

--- deployment/aws/modules/db/main.tf
+++ deployment/aws/modules/db/main.tf
@@ -3,10 +3,10 @@ data "aws_ami" "db_ami" {
 
   filter {
     name   = "name"
-    values = ["debian-stretch-hvm-x86_64-gp2-*"]
+    values = ["multicloud-aws-db-*"]
   }
 
-  owners = ["379101102735"]
+  owners = ["640680520898"]
 }
 
 resource "aws_instance" "db" {
