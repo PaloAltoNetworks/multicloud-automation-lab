95bcf864d4f47f5db7630051d97e66388c35a545
Michael Richardson <mirichardson@paloaltonetworks.com>
2019-05-06 09:21:33 -0500

    Update to AWS Terraform config. (#7)
    * Switch to static AMIs.

---
+++ deployment/aws/modules/bootstrap/variables.tf
@@ -0,0 +1,5 @@
+variable "bootstrap_bucket_name" {}
+
+variable "bootstrap_xml_path" {}
+
+variable "bootstrap_init_cfg_path" {}
---
+++ deployment/aws/modules/web/outputs.tf
@@ -0,0 +1,3 @@
+output "web_instance_id" {
+  value = "${aws_instance.web.id}"
+}
